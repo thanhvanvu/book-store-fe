9dd9e2cf2e35eedb1ab47e8f6724afa7a78a66e6
Thanh Vu <65506586+vvt4994@users.noreply.github.com>
2023-05-23 09:50:32 -0500

    add more scss for Register page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -15,103 +15,110 @@ const Register = () => {
 
   return (
     <>
-      <Col className="register-page" span={12}>
-        <h3 className="register-title">Register</h3>
-        <Form
-          name="register"
-          labelCol={{ span: 7 }}
-          wrapperCol={{ span: 17 }}
-          style={{ maxWidth: 500 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
-        >
-          <Form.Item
-            label="Full Name"
-            name="fullName"
-            rules={[
-              { required: true, message: 'Please input your full name!' },
-            ]}
+      <div className="register-wrapper">
+        <Col className="register-page" span={10}>
+          <h3 className="register-title">Register</h3>
+          <Form
+            name="register"
+            labelCol={{ span: 7 }}
+            wrapperCol={{ span: 24 }}
+            style={{ maxWidth: 400 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
           >
-            <Input />
-          </Form.Item>
+            <Form.Item
+              labelCol={{ span: 24 }}
+              label="Full Name"
+              name="fullName"
+              rules={[
+                { required: true, message: 'Please input your full name!' },
+              ]}
+            >
+              <Input />
+            </Form.Item>
 
-          <Form.Item
-            name="email"
-            label="E-mail"
-            rules={[
-              {
-                type: 'email',
-                message: 'The input is not valid E-mail!',
-              },
-              {
-                required: true,
-                message: 'Please input your E-mail!',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            name="password"
-            label="Password"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your password!',
-              },
-            ]}
-            hasFeedback
-          >
-            <Input.Password />
-          </Form.Item>
-
-          <Form.Item
-            name="confirm"
-            label="Confirm Password:"
-            dependencies={['password']}
-            hasFeedback
-            rules={[
-              {
-                required: true,
-                message: 'Please confirm your password!',
-              },
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (!value || getFieldValue('password') === value) {
-                    return Promise.resolve()
-                  }
-                  return Promise.reject(
-                    new Error(
-                      'The two passwords that you entered do not match!'
-                    )
-                  )
+            <Form.Item
+              labelCol={{ span: 24 }}
+              name="email"
+              label="E-mail"
+              rules={[
+                {
+                  type: 'email',
+                  message: 'The input is not valid E-mail!',
                 },
-              }),
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
+                {
+                  required: true,
+                  message: 'Please input your E-mail!',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
 
-          <Form.Item
-            name="phone"
-            label="Phone Number"
-            rules={[
-              { required: true, message: 'Please input your phone number!' },
-            ]}
-          >
-            <Input style={{ width: '100%' }} />
-          </Form.Item>
+            <Form.Item
+              labelCol={{ span: 24 }}
+              name="password"
+              label="Password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+              ]}
+              hasFeedback
+            >
+              <Input.Password />
+            </Form.Item>
 
-          <Form.Item wrapperCol={{ offset: 10, span: 0 }}>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </Col>
+            <Form.Item
+              labelCol={{ span: 24 }}
+              name="confirm"
+              label="Confirm Password:"
+              dependencies={['password']}
+              hasFeedback
+              rules={[
+                {
+                  required: true,
+                  message: 'Please confirm your password!',
+                },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue('password') === value) {
+                      return Promise.resolve()
+                    }
+                    return Promise.reject(
+                      new Error(
+                        'The two passwords that you entered do not match!'
+                      )
+                    )
+                  },
+                }),
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item
+              labelCol={{ span: 24 }}
+              name="phone"
+              label="Phone Number"
+              rules={[
+                { required: true, message: 'Please input your phone number!' },
+              ]}
+            >
+              <Input style={{ width: '100%' }} />
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 10, span: 0 }}>
+              <Button type="primary" htmlType="submit">
+                Register
+              </Button>
+            </Form.Item>
+          </Form>
+        </Col>
+      </div>
     </>
   )
 }
