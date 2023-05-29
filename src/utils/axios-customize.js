d1133c58a4ba348dd56f9569c90ca62ba25b774d
Thanh Vu <65506586+vvt4994@users.noreply.github.com>
2023-05-29 16:26:43 -0500

    28: Axios retry - handle refresh token

--- src/utils/axios-customize.js
+++ src/utils/axios-customize.js
@@ -12,6 +12,16 @@ const instance = axios.create({
 const access_token = localStorage.getItem('access_token')
 instance.defaults.headers.common = { Authorization: `Bearer ${access_token}` }
 
+// update token
+const handleRefreshToken = async () => {
+  const response = await instance.get('/api/v1/auth/refresh')
+  if (response?.data) {
+    return response.data.access_token
+  } else {
+    return null
+  }
+}
+
 // Add a request interceptor
 instance.interceptors.request.use(
   function (config) {
@@ -24,6 +34,8 @@ instance.interceptors.request.use(
   }
 )
 
+const NO_RETRY_HEADER = 'x-no-retry'
+
 // Add a response interceptor
 instance.interceptors.response.use(
   function (response) {
@@ -31,9 +43,27 @@ instance.interceptors.response.use(
     // Do something with response data
     return response && response.data ? response.data : response
   },
-  function (error) {
+  async function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
+
+    if (
+      error.config &&
+      error.response &&
+      +error.response.status === 401 &&
+      !error.config.headers[NO_RETRY_HEADER]
+    ) {
+      const access_token = await handleRefreshToken()
+      error.config.headers[NO_RETRY_HEADER] = 'true'
+      if (access_token) {
+        error.config.headers['Authorization'] = `Bearer ${access_token}`
+
+        localStorage.setItem('access_token', access_token)
+
+        return instance.request(error.config)
+      }
+    }
+
     return error?.response?.data ?? Promise.reject(error)
   }
 )
